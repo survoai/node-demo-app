21b92cd1187f5e1406e256b55819d03bce31a30a
Rajesh Tilwani <rajesh.tilwani3@gmail.com>
2021-09-02 00:20:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res, next) => {
 });
 
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(80, () => {
+ console.log("Server running on port 80");
 });
