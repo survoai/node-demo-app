279caf1acbe73b5eb71313f2e7880e8d828c9eae
Rajesh Tilwani <rajesh.tilwani3@gmail.com>
2021-09-16 12:26:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
- res.json(["Howdy","Lisa","Michael","Ginger","Food"]);
+ res.json(["ssup","Lisa","Michael","Ginger","Food"]);
 });
 
 
