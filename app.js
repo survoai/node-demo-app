b1849ad6a2caf1f21ddc213423311a2f52631909
Rajesh Tilwani <rajesh.tilwani3@gmail.com>
2021-10-07 20:37:51 +0530

    humalect

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
- res.json(["Jackson6","Lisa","Michael","Ginger","Drinks"]);
+ res.json(["Jackson61","Lisa","Michael","Ginger","Drinks"]);
 });
 
 
