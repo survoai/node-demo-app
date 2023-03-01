7d859981c83bd44ea25c599f430e3bb39f7c69d3
Rajesh Tilwani <rajesh.tilwani3@gmail.com>
2023-03-01 16:09:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
- res.json(["rajesh","hello","hello","Ginger","Rajeshss"]);
+ res.json(["Sunflower","Rose","Lily","Marigold","Orchids"]);
 });
-
 
 app.listen(80, () => {
  console.log("Server running on port 80");
